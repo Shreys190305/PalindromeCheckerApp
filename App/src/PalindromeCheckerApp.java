c882b989f5c206ed5f7725172ca9ad3f6a017040
Shreys190305 <sr3387@srmist.edu.in>
2026-02-27 14:40:23 +0530

    Printing Palindrome

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+/**
+ * Application entry point
+ *
+ * This is a Java Palindrome Checker App Welcome Message
+ *
+ * @author Developer
+ * @Version 1.0
+ */
+public class PalindromeCheckerApp {
+  public static void main(String[] args){
+      System.out.println("Welcome to the Palindrome Checker Management System");
+      System.out.println("Version : 1.0");
+   System.out.println("System initialized successfully.");}
+}
